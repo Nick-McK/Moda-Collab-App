d087b1610486f0fed506872c9db12d3014eb1e49
nm125 <nm125@hw.ac.uk>
2022-01-23 20:15:04 +0000

    Added nodemon as dev dependency + collab rooms

--- Homepage_serverside.js
+++ Homepage_serverside.js
@@ -9,25 +9,72 @@ const router = express.Router();
 const path = require("path");
 
 // Use files from within the file structure
-app.use(express.static(__dirname));
-app.use(express.static("public"));
+app.use(express.static(__dirname)); // Serves html files
+app.use(express.static(__dirname + "/public")); //serving style sheets and js files
+app.use(express.static(__dirname + "/public/assets")); // different assets for pages
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+
+let rooms = {};
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/Homepage.html"));
 });
 
-app.get("/collab_room", (req, res) => {
+// app.get("/collab_room", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/collab_room.html"));
+// });
+
+// Handles the post event which takes us to the collab_room that we create
+app.post("/collab_room", (req, res) => {
+    if (rooms[req.body.roomName] != null) {
+        return res.redirect("/");   // This closes the collab menu currently, figure out way to keep it open
+    }
+    rooms[req.body.roomName] = {users: {} };
+    console.log(rooms);
+    res.redirect("/collab_room/" + req.body.roomName);
+    // This is duplicated lower down, for some reason when using only 1 the rooms dont load until a refresh or
+    // They only load when you create them and are lost when refreshed
+    let roomList = new Array();
+    Object.keys(rooms).forEach(room => {
+        if (!roomList.includes(room)) {
+            roomList.push(room);
+            console.log("roomList", roomList);
+            io.emit("roomNames", roomList);
+        }
+    });
+})
+
+
+app.get("/collab_room/:roomName", (req, res) => {
+    // console.log("rooms", rooms);
     res.sendFile(__dirname + "/collab_room.html");
-});
+})
 
 
 
-
+// When we connect give every use the rooms available
 io.on('connect', (socket) => {
     console.log("A user connected", socket.id);
+    let roomList = new Array();
+    Object.keys(rooms).forEach(room => {
+        if (!roomList.includes(room)) {
+            roomList.push(room);
+            console.log("roomList", roomList);
+            io.emit("roomNames", roomList);
+        }
+    });
+
+    
 
 
-    socket.on("start", (req, res) => {
+    socket.on("start", (answer) => {
+        
+
+
+
         io.emit("collabTime");
         // res.sendFile(__dirname + "/collab_room.html");
     });
--- public/Homepage.js
+++ public/Homepage.js
@@ -4,6 +4,7 @@ const socket = io();
 const collabContainer = document.getElementById("_collabContainer");
 const promptContainer = document.getElementById("_promptContainer");
 const promptBtn = document.getElementById("promptButton");
+let promptAnswer = document.getElementById("promptAnswer");
 const btnOpen = document.getElementById("collaborate");
 const btnClose = document.getElementById("close");
 const startCollab = document.getElementById("startCollaborating");
@@ -11,11 +12,17 @@ const addCollab = document.getElementById("addCollaborators");
 
 // Events for handling opening and closing the collaboration menu
 btnOpen.onclick =  () => {
-    collabContainer.style.display = "flex"
+    if (collabContainer.style.display == "flex") {
+        collabContainer.style.display = "none";
+        promptContainer.style.display = "none"; // Set the prompt container to hide if we close the collab menu
+    } else {
+        collabContainer.style.display = "flex";
+    }
 };
 
 btnClose.onclick = () => {
     collabContainer.style.display = "none";
+    promptContainer.style.display = "none";
 }
 
 startCollab.addEventListener("click", () => {
@@ -25,22 +32,38 @@ startCollab.addEventListener("click", () => {
 
 
 
-
-// Users to be added to the chat
-let users = new Array();
-
-
-
 // Collaboration menu start and add collaborators buttons
 socket.on("connect", () => {
-    promptBtn.addEventListener("click", () => {
-        socket.emit("start");
-    });
+    
 });
 
-socket.on("collabTime", () => {
-    window.location = "/collab_room.html";
+let recordedRooms = new Array();
+// Adding rooms to the collab menu
+socket.on("roomNames", (roomList) => {
+    for (let room of roomList) {
+        const collabContent = document.getElementById("_collabContent");
+        let roomDiv = document.createElement("div");
+        let roomLink = document.createElement("a");
+        let linkTitle = document.createTextNode("Join");
+        roomDiv.classList.add("collabRoom");
+        roomDiv.innerHTML = room;
+
+        roomLink.appendChild(linkTitle);
+        roomLink.href = "/collab_room/" + room;
+        if (!recordedRooms.includes(room)) {
+            collabContent.appendChild(roomDiv);
+            roomDiv.appendChild(roomLink);
+        }
+        recordedRooms.push(room);
+    }
 })
+
+
+
+
+// socket.on("collabTime", () => {
+//     window.location = "/collab_room.html";
+// })
     
 
 addCollab.onclick = () => {
