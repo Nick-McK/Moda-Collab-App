13587d7840ba12c6039ab4988e0f5455e4ccd629
Daniel Robertson <dr62@hw.ac.uk>
2022-02-18 16:40:33 +0000

    Implemented design thumbnails

--- public/collab_room_clientside.js
+++ public/collab_room_clientside.js
@@ -552,7 +552,9 @@ socket.on("chatMessage", data => {
 
 // Emit to server design JSON data to be stored in a file for saving
 function saveDesign() {
-    socket.emit('saveDesign', {design: JSON.stringify(canvas)})
+    socket.emit('saveDesign', {design: JSON.stringify(canvas), thumbnail: canvas.toDataURL({format: 'jpeg'})});
+    var win = window.open();
+    win.document.write('<iframe src="' + canvas.toDataURL({format: 'jpeg'})  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'); // this gives a preview of the image, can be commented out if needs be
 }
 
 // Recieves alert on success of save
