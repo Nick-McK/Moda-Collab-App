e0bac41c087ea0ca460ec6452e0e1f1a66542c01
Daniel Bartmann <dbartmann16@gmail.com>
2022-03-18 23:35:31 +0000

    fixed css in login register links on landing page

--- public/Profile.js
+++ public/Profile.js
@@ -8,17 +8,9 @@ class Profile {
         return this.profile;
     }
 }
-<<<<<<< Updated upstream
 
 con.query("SELECT (whatever) FROM posts WHERE userUD = ?", [username], (err, result) => {
 
 });
 
-=======
-con.query("SELECT (whatever) FROM posts WHERE userUD = ?", [username], (err, result) => {
-    const postImages = document.querySelectorAll(".post_img");
-    const postTop = document.querySelectorAll(".post-top");
-    const postBar = document.querySelectorAll(".post-bar")
-});
->>>>>>> Stashed changes
 export {Profile};
